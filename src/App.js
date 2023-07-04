972c9cb54d8dd07d5a5cef9d72b588da712d9a47
mnemokaz <mnemokaz@gmail.com>
2023-07-04 13:06:21 +0300

    Landing page done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/Menu/Menu';
+import Landing from './components/Landing/Landing';
+import Blog from './components/Blog/Blog';
+
+
 
 
 
 function App() {
   return (
 <div className = 'container'> 
+
   <Menu/>
+  {/* <Landing /> */}
+  {/*<Blog/>*/}
+ 
  </div>
   )
    
