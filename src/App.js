c90a06ab6c6e952b5150cf6f442b7172767a5617
mnemokaz <mnemokaz@gmail.com>
2023-07-05 16:34:58 +0300

    shop page done

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import './App.css';
 import Menu from './components/Menu/Menu';
 import Landing from './components/Landing/Landing';
 import Blog from './components/Blog/Blog';
+import Shop from './components/Shop/Shop';
+import { useState } from 'react';
 
 
 
 
 
 function App() {
+  const [page, setPage] = useState('landing')
+
   return (
 <div className = 'container'> 
 
-  <Menu/>
-  {/* <Landing /> */}
-  {/*<Blog/>*/}
- 
+  <Menu onClick = {setPage}/>
+  {page === 'landing' &&  <Landing />}
+  { page === 'blog' && <Blog/>}
+  { page === 'shop' && <Shop/>}
  </div>
   )
    
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,12 +1,13 @@
 import styles from './Menu.module.css'
 
-function Menu() {
+function Menu({onClick}) {
+
   return (
  <form className={styles.container}>
         <h2 className={styles.header}>Выберите сетку сайта</h2>
 
        
-        <div className={styles.button}>
+        <div className={styles.button} onClick={() => onClick('landing')}>
           <span className={styles.text}>Лендинг</span>
           <svg className={styles.img} width="240" height="132" viewBox="0 0 240 132" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.3" y="0.3" width="239.4" height="15.4" strokeWidth="0.6" strokeDasharray="5 5"/>
@@ -16,7 +17,7 @@ function Menu() {
         </div>
 
 
-        <div className={styles.button}>
+        <div className={styles.button} onClick={() => onClick('blog')}>
        
           <span className={styles.text}>Блог</span>
           <svg className={styles.img} width="240" height="132" viewBox="0 0 240 132" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -28,7 +29,7 @@ function Menu() {
         </div>
 
 
-        <div className={styles.button}>
+        <div className={styles.button} onClick={() => onClick('shop')}>
           <span className={styles.text}>Магазин</span>
           <svg className={styles.img} width="240" height="132" viewBox="0 0 240 132" fill="none" xmlns="http://www.w3.org/2000/svg">
             <rect x="0.3" y="0.3" width="239.4" height="15.4" strokeWidth="0.6" strokeDasharray="5 5"/>
